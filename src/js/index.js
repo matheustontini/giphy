ac0e05b4d886ca90316fb348cac621a365b8cd68
Matheus Tontini <tontini92@gmail.com>
2019-07-28 18:07:00 +0200

    Splited functions and SCSS elements

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-const key = "tERZTBpegoO9vTp535UAmVHO9JHrMNA0";
+const giphyApiKey = "tERZTBpegoO9vTp535UAmVHO9JHrMNA0";
 let offset = 0;
 let type = "trending";
 let searchValue = "";
@@ -10,9 +10,9 @@ const title = document.querySelector("h1");
 const loading = document.querySelector(".loading");
 
 // Function called when the website is loaded, click to see more results or search for something
-const loadGif = () => {
+const loadGif = (type, searchValue, offset) => {
   // Mount a API URL with the type (serach or trending), key, and the offset
-  let url = `https://api.giphy.com/v1/gifs/${type}?api_key=${key}&offset=${offset}&limit=10`;
+  let url = `https://api.giphy.com/v1/gifs/${type}?api_key=${giphyApiKey}&offset=${offset}&limit=10`;
 
   // Check if the user is searching to add a string to the API URL
   if (type == "search") {
@@ -28,20 +28,9 @@ const loadGif = () => {
       // Check if the results are less the one to hide the button load
       if (content.data.length < 1) {
         btnLoad.style.display = "none";
-
-        // Check if the search did not find any results to show a "no results" message
-        if (gifContent.childNodes.length === 0) {
-          const notice = document.createElement("p");
-          notice.textContent = "Results not found";
-          gifContent.appendChild(notice);
-        }
+        setResultNotFound();
       } else {
-        // Create an image element to each result
-        content.data.forEach(data => {
-          const img = document.createElement("img");
-          img.src = data.images.downsized.url;
-          gifContent.appendChild(img);
-        });
+        createGiphys(content.data);
       }
 
       // Hide the "loading" display
@@ -50,6 +39,24 @@ const loadGif = () => {
     .catch(err => {
       console.error(err);
     });
+};
+
+// Check if the search did not find any results to show a "no results" message
+const setResultNotFound = () => {
+  if (gifContent.childNodes.length === 0) {
+    const notice = document.createElement("p");
+    notice.textContent = "Results not found";
+    gifContent.appendChild(notice);
+  }
+};
+
+// Create an image element to each result
+const createGiphys = data => {
+  data.forEach(data => {
+    const img = document.createElement("img");
+    img.src = data.images.downsized.url;
+    gifContent.appendChild(img);
+  });
 };
 
 // Function called when the user press ENTER or click on search button
@@ -63,20 +70,20 @@ const handleSearch = () => {
   // Save the search value in a variable to be able to clean to search input
   searchValue = search.value.trim();
   // Call the loadGif function to show teh results
-  loadGif();
+  loadGif(type, searchValue, offset);
   // Clean the search input
   search.value = "";
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   // Call the loadGif function when the website is loaded
-  loadGif();
+  loadGif(type, searchValue, offset);
 
   btnLoad.addEventListener("click", () => {
     // Set a offset to specifie the starting position of the new results
     offset += 10;
     // Call the loadGif function to show more results
-    loadGif();
+    loadGif(type, searchValue, offset);
   });
 
   btnSearch.addEventListener("click", handleSearch);
